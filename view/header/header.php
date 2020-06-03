43d881e02678ba189023fa4c64e8ecf052a724cc
Surya235Dev <37703729+Surya235Dev@users.noreply.github.com>
2020-06-03 22:38:44 +0530

    Add files via upload

---
+++ view/header/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="public/script.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/style.css">
+    <title>MVC | OOP | Surya </title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light justify-content-between" style="margin-bottom:40px;">
+  <a class="navbar-brand">Shoppers Stop - Surya</a>
+  <form class="form-inline">
+   <a href="index.php?controller=product&action=add" class="btn btn-primary">Add a  New Product </a>
+  </form>
+</nav>
