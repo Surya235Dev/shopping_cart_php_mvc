75939cf374ea5715a3d86afa204511bbab99e494
Surya235Dev <37703729+Surya235Dev@users.noreply.github.com>
2020-06-03 22:31:23 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,19 @@
+let cart = document.getElementsByClassName('cart');
+let len = cart.length;
+let items = [];
+
+for(let i = 0; i<len;i++){
+  let d =document.getElementsByClassName('cart')[i]
+   d.addEventListener('click',()=>{
+     let cost = document.getElementsByClassName('cart')[i].value;
+     items.push(parseInt(cost));
+
+    let total = items.reduce((previous, current)=>{
+  return previous + current;
+    },0)
+
+  const dom = document.getElementById('total');
+  dom.style.display="block";
+  dom.textContent = `Total : ${total}`
+   })
+}
