8936d7b902ad3efc9a3990575c066c386f50a9b3
Surya235Dev <37703729+Surya235Dev@users.noreply.github.com>
2020-06-04 12:20:23 +0530

    Update index.php

--- controller/index.php
+++ controller/index.php
@@ -1,4 +1,7 @@
 <?php 
+   include "operation.php";
+
+   $newApp = new Operation();
    if(isset($_GET['action'])){
        $action = $_GET['action'];
    } else {
@@ -6,67 +9,35 @@
    }
    
    switch($action){
-       case "add" :
-               
-               if(isset($_POST['submit'])){
-                   $name = $_POST['name'];
-                   $price = $_POST['price'];
-                   $description = $_POST['description'];
-                   $imageurl = $_POST['imageurl'];
-                   
-                   if(empty($name) || empty($price) || empty($description) || empty($imageurl) ){
-                       $showAlert = 'Invalid Input. Please fill the form again.';
-                   } else {  
-                       
-                       $app->action("INSERT INTO items (name, price, description, imageurl) VALUES ('$name','$price','$description','$imageurl')");
-                       header("Location:index.php?controller=product");
-                   }
-               }
-               require_once "view/addproduct.php";
-            break;
+       case "add" : $alert = $newApp->store_record();
+                    if(isset($alert)){
+                        $showAlert = $alert;
+                    }
+                    require_once "view/addproduct.php";
+                    break;
             
        case "edit" : 
-              
-              if(isset($_GET['id'])){
-              
-                  $id = $_GET['id'];
-                  
-                  $result = $app->executeQuery("SELECT * FROM items WHERE id='$id'");
-                  foreach($result as $item){
+              if(isset($_GET['id'])){ 
+                 $id = $_GET['id'];
+                 $result = $newApp->get_record($id);
+                 foreach($result as $item){
                       $name = isset($item['name'])?$item['name']:'';
                       $price = isset($item['price'])?$item['price']:'';
                       $description = isset($item['description'])?$item['description']:'';
                       $imageurl =isset($item['imageurl'])?$item['imageurl']:'';
+                      $id = $item['id'];
                   }   
               }
+            $alert = $newApp->update();
+            if(isset($alert)){
+                $showAlert = $alert;
+            }
+            require_once "view/editproduct.php";
+            break;
    
-              if(isset($_POST['submit'])){
-                  
-                  $new_name = $_POST['name'];
-                  $new_price = $_POST['price'];
-                  $new_description = $_POST['description'];
-                  $new_imageurl = $_POST['imageurl'];
+       case "delete": $newApp->delete_record($_GET['id']); break;
    
-                  if(empty($new_name) || empty($new_imageurl) || empty($new_price) || empty($new_description)){
-                      $showAlert = 'Invalid Input. Please fill the form again.';
-                  } else {
-                      $app->action("UPDATE items SET name='$new_name', price='$new_price', description='$new_description', imageurl='$new_imageurl' WHERE id='$id' ");
-                      header("Location:index.php?controller=product");
-                  }
-              }
-              require_once "view/editproduct.php";
-              break;
-   
-       case "delete":
-               if(isset($_GET['id'])){
-                   $id=$_GET['id'];
-                   $app->action("DELETE from items where id='$id' ");
-                   header("Location:index.php?controller=product");
-               }
-                break;
-   
-   
-       default: $data = $app->executeQuery("SELECT * from items");
+       default: $data = $newApp->executeQuery("SELECT * from items");
                 require_once "view/index.php";
                 break;
    }
